13eacf519b6b74c23a7e043316b97e608fdefdae
Slowcobo <spencer.coleman92@gmail.com>
2020-06-22 13:23:06 -0700

    Add Links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import React from "react";
-import "./App.css";
-
+import { Switch, Route } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { NavLink } from "react-router-dom";
 import Content from "./Content";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
-      <Content />
+      <Navbar expand="sm">
+        <Navbar.Brand href="#home">Count to Ten</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link>
+              <NavLink
+                exact
+                to="/"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Home
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink
+                exact
+                to="/about"
+                className="nav-link"
+                activeClassName="active"
+              >
+                About
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink
+                exact
+                to="/books"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Books
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink
+                exact
+                to="/contact"
+                className="nav-link"
+                activeClassName="active"
+              >
+                Contact
+              </NavLink>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+        <Route exact path="/" render={() => <Content />} />
+        <Route exact path="/about" render={() => <h1>About Page </h1>} />
+        <Route exact path="/books" render={() => <h1>Book Page</h1>} />
+        <Route exact path="/contact" render={() => <h1>Contact Page</h1>} />
+      </Switch>
     </div>
   );
 }
