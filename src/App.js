38763915e635de67d5306f58b0536cd14a846a34
Slowcobo <spencer.coleman92@gmail.com>
2020-06-22 20:05:22 -0700

    Improve About formatting

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 import Home from "./home/Home";
 import About from "./about/About";
 import Books from "./books/Books";
@@ -12,60 +13,62 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Navbar expand="sm">
-        <Navbar.Brand href="#home">Count to Ten</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link>
-              <NavLink
-                exact
-                to="/"
-                className="nav-link"
-                activeClassName="active"
-              >
-                Home
-              </NavLink>
-            </Nav.Link>
-            <Nav.Link>
-              <NavLink
-                exact
-                to="/about"
-                className="nav-link"
-                activeClassName="active"
-              >
-                About
-              </NavLink>
-            </Nav.Link>
-            <Nav.Link>
-              <NavLink
-                exact
-                to="/books"
-                className="nav-link"
-                activeClassName="active"
-              >
-                Books
-              </NavLink>
-            </Nav.Link>
-            <Nav.Link>
-              <NavLink
-                exact
-                to="/contact"
-                className="nav-link"
-                activeClassName="active"
-              >
-                Contact
-              </NavLink>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Switch>
-        <Route exact path="/" render={() => <Home />} />
-        <Route exact path="/about" render={() => <About />} />
-        <Route exact path="/books" render={() => <Books />} />
-        <Route exact path="/contact" render={() => <Contact />} />
-      </Switch>
+      <Container>
+        <Navbar expand="sm">
+          <Navbar.Brand href="#home">Count to Ten</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link>
+                <NavLink
+                  exact
+                  to="/"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Home
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink
+                  exact
+                  to="/about"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  About
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink
+                  exact
+                  to="/books"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Books
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link>
+                <NavLink
+                  exact
+                  to="/contact"
+                  className="nav-link"
+                  activeClassName="active"
+                >
+                  Contact
+                </NavLink>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/about" render={() => <About />} />
+          <Route exact path="/books" render={() => <Books />} />
+          <Route exact path="/contact" render={() => <Contact />} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/about/About.js
+++ src/about/About.js
@@ -1,25 +1,44 @@
 import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import profile from "./profile.png";
+import "./About.css";
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <h1>Image Placeholder</h1>
-        <p>
-          Joan Parnell is an Early Childhood Educator and the lead
-          Pre-Kindergarten teacher at Lakeside Baptist Church in Granbury,
-          Texas. Working with young children in a classroom setting gives her
-          the inspiration for her “Zowen” books. Joan graduated from the
-          University of Texas at Austin with a B.S. degree in Elementary
-          Education. She was born and raised in Houston, Texas, but now resides
-          in the beautiful, peaceful town of Glen Rose, Texas. She is the mother
-          of two boys, of whom she is very proud. She has two Australian
-          Shepherds, one Cairn Terrier, and a horse named Shooter. Joan will
-          tell you that she has been blessed in so many ways. She loves her
-          family, her job, the children that she teaches, and her director and
-          co-workers that lift her up daily. She gives all the glory to her Lord
-          and Savior, Jesus Christ!
-        </p>
+      <div className="About">
+        <Row>
+          <h1 className="About-title">About Joan</h1>
+        </Row>
+        <Row>
+          <Col xs={12} md={4}>
+            <Image className="profile" src={profile} />
+          </Col>
+          <Col>
+            <p>
+              Joan Parnell is an Early Childhood Educator and the lead
+              Pre-Kindergarten teacher at Lakeside Baptist Church in Granbury,
+              Texas. Working with young children in a classroom setting gives
+              her the inspiration for her “Zowen” books.
+            </p>
+            <p>
+              Joan graduated from the University of Texas at Austin with a B.S.
+              degree in Elementary Education. She was born and raised in
+              Houston, Texas, but now resides in the beautiful, peaceful town of
+              Glen Rose, Texas. She is the mother of two boys, of whom she is
+              very proud. She has two Australian Shepherds, one Cairn Terrier,
+              and a horse named Shooter.
+            </p>
+            <p>
+              Joan will tell you that she has been blessed in so many ways. She
+              loves her family, her job, the children that she teaches, and her
+              director and co-workers that lift her up daily. She gives all the
+              glory to her Lord and Savior, Jesus Christ!
+            </p>
+          </Col>
+        </Row>
       </div>
     );
   }
