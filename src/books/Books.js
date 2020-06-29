582b26f462dea5bd0af352eb6683e2fa3bcf18b5
Slowcobo <spencer.coleman92@gmail.com>
2020-06-29 12:43:24 -0700

    Fix Book Formatting

--- src/books/Books.js
+++ src/books/Books.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
@@ -9,36 +10,44 @@ import "./Books.css";
 class Books extends Component {
   render() {
     return (
-      <div className="Books">
-        <Col sm={6}>
+      <div>
+        <Container>
           <Row>
-            <h1 className="page-title">Books</h1>
-          </Row>
-          <Row>
-            <Col md={4}>
-              <Image className="Book-img" src={zowenmakes10} />
-            </Col>
             <Col>
-              <div>
-                <h4 className="Book-title">And Zowen Makes 10!</h4>
-                <p>
-                  The day has finally arrived. It’s the class field trip to the
-                  Zoo. The children are so excited to see all the animals, but
-                  Zowen has one thing on his mind. “Will there be something to
-                  eat?”
-                </p>
-                <p>
-                  Join Zowen and his classmates as they visit all the
-                  fascinating animals at the zoo. Upon leaving the reptile
-                  house, Miss Beverly lines up her class to count everyone “just
-                  to be sure.” Much to her dismay, she and the children discover
-                  that Zowen is missing! Where could he be?
-                </p>
-              </div>
-              <Button>Buy the Book</Button>
+              <h1 className="page-header">
+                The <span>Books</span>
+              </h1>
             </Col>
           </Row>
-        </Col>
+        </Container>
+        <div className="Books-content">
+          <Container>
+            <Row>
+              <Col md={4}>
+                <Image className="Book-img" src={zowenmakes10} />
+              </Col>
+              <Col>
+                <div>
+                  <h4 className="Book-title">And Zowen Makes 10!</h4>
+                  <p>
+                    The day has finally arrived. It’s the class field trip to
+                    the Zoo. The children are so excited to see all the animals,
+                    but Zowen has one thing on his mind. “Will there be
+                    something to eat?”
+                  </p>
+                  <p>
+                    Join Zowen and his classmates as they visit all the
+                    fascinating animals at the zoo. Upon leaving the reptile
+                    house, Miss Beverly lines up her class to count everyone
+                    “just to be sure.” Much to her dismay, she and the children
+                    discover that Zowen is missing! Where could he be?
+                  </p>
+                </div>
+                <Button>Buy the Book</Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     );
   }
