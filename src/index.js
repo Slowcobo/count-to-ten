e2151c312a8e16fdfa03b8fe02b31a928f02b0de
Slowcobo <spencer.coleman92@gmail.com>
2020-06-30 15:51:21 -0700

    Add ScrollToTop to Routes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
